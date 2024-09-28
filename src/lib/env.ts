82e9e9e0f52d890bec36d99dea5e2637e63f0a87
Romain BOBOE <rboboe@gmail.com>
2024-09-28 13:59:57 -0400

    envirionnement variable fix

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -9,6 +9,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
+    NEXTAUTH_URL: z.string().url(),
     GITHUB_ID: z.string().optional(),
     GITHUB_SECRET: z.string().optional(),
     TWITTER_ID: z.string().optional(),
@@ -22,6 +23,8 @@ export const env = createEnv({
     NODE_ENV: z.enum(["development", "production", "test"]),
     STRIPE_WEBHOOK_SECRET: z.string().optional(),
     NEXTAUTH_SECRET: z.string().min(1),
+    API_KEY_SECRET: z.string().min(1),
+    WIDGET_SECRET_KEY: z.string().min(1),
   },
   /**
    * If you add `client` environment variables, you need to add them to
@@ -30,10 +33,20 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().min(1),
     NEXT_PUBLIC_EMAIL_CONTACT: z.string().min(1),
+    NEXT_PUBLIC_BASE_URL: z.string().url(),
+    NEXT_PUBLIC_SERVER_URL: z.string().url().optional(),
+    NEXT_PUBLIC_WIDGET_URL: z.string().url(),
+    NEXT_PUBLIC_API_URL: z.string().url(),
+    NEXT_PUBLIC_UPLOADTHING_SECRET: z.string().optional(),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     NEXT_PUBLIC_EMAIL_CONTACT: process.env.NEXT_PUBLIC_EMAIL_CONTACT,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+    NEXT_PUBLIC_WIDGET_URL: process.env.NEXT_PUBLIC_WIDGET_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_UPLOADTHING_SECRET: process.env.NEXT_PUBLIC_UPLOADTHING_SECRET,
   },
 });
