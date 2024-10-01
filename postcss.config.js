2b226514198327045ccc28b65ca9b94ad45cb532
Romain BOBOE <rboboe@gmail.com>
2024-09-30 22:07:54 -0400

    Pushing style for the sidepanelcontent

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+ const purgecss = require('@fullhuman/postcss-purgecss')({
+     content: ['./src/**/*.tsx', './public/index.html'],
+     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+     safelist: ['now-widget-*'],
+   });
+
+   module.exports = {
+     plugins: [
+       require('postcss-import'),
+       require('tailwindcss'),
+       ...(process.env.NODE_ENV === 'production' ? [purgecss, require('cssnano')] : []),
+       require('autoprefixer'),
+     ],
+   };
