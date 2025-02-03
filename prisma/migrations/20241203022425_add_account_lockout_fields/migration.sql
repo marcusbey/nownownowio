ddfec8d3bd3060a127f39cb0f9af5eeb76c46974
Romain BOBOE <rboboe@gmail.com>
2025-02-02 21:09:45 -0500

    chore: remove old migrations
    These migrations have been squashed into a single migration file

--- prisma/migrations/20241203022425_add_account_lockout_fields/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "failedAttempts" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "lockedUntil" TIMESTAMP(3);
--- prisma/migrations/20250116_add_website_url/migration.sql
+++
@@ -1,2 +0,0 @@
--- Add websiteUrl to User table
-ALTER TABLE "User" ADD COLUMN "websiteUrl" TEXT;
